95e18cd54346575a3c8fa129d5c4c5b76120be14
eddysantiago <santiiagoobregon2009@gmail.com>
2025-07-21 11:50:47 -0500

    Project creation and running

---
+++ APP ALMACENAMIENTO/src/main/java/com/example/app_almacenamiento/AppAlmacenamientoApplication.java
@@ -0,0 +1,13 @@
+package com.example.app_almacenamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAlmacenamientoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppAlmacenamientoApplication.class, args);
+    }
+
+}
---
+++ APP ALMACENAMIENTO/src/test/java/com/example/app_almacenamiento/AppAlmacenamientoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.app_almacenamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAlmacenamientoApplicationTests {
+
+    @Test
+
+    void contextLoads() {
+        // work later
+    }
+
+}
